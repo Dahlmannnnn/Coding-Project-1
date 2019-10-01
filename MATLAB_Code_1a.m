dbfe53d30fcb43fc44a195d32929d4a5a133977b
Dahlmannnnn <54683052+Dahlmannnnn@users.noreply.github.com>
2019-10-01 12:21:22 -0700

    Create MATLAB_Code_1a.m
    Inital Comments

---
+++ MATLAB_Code_1a.m
@@ -0,0 +1,17 @@
+% Alexander Dahlmann
+% EE599 Machine Learning - Fall 2019
+% Coding Project 1
+% Using Blood Pressure to Identify Risk of Heart Disease
+
+
+% Input Data Set - Can I download directly from the website? - Store size
+% of vector in variable n
+
+
+% create Vector (v) of size n and establish kernels (k) at each datapoint
+
+
+% Sum Values of Kernels to create equation Y
+
+
+% Output Graph n,k, and Y/10 [This allows the data to be displayed ]
